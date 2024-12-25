dac30afc0162f1e6615d3a77e1a462230334bb53
Oleksandr <olukianets@lytvynov-production.com>
2024-12-25 21:09:29 +0100

    beatify code

---
+++ src/core/insta_client/__init__.py
@@ -0,0 +1,1 @@
+from core.insta_client.client import InstaClient
--- src/core/scripts/leave_comments.py
+++ src/core/scripts/leave_comments.py
@@ -1,9 +1,9 @@
-from instagrapi.types import Media
+from instagrapi.types import Media, User
 
 from config import settings
 from logger import logger
 
-from core.insta_client.client import InstaClient
+from core.insta_client import InstaClient
 
 COUNT_POSTS: int = 3
 
@@ -11,9 +11,7 @@ COUNT_POSTS: int = 3
 async def leave_comments():
     client: InstaClient = InstaClient()
     user: User = client.user_info_by_username(settings.COMMENT_USERNAME)
-    user_id: str = user.pk
-
-    posts: list[Media] = client.user_medias(user_id, amount=COUNT_POSTS)
-    for post in posts:
-        client.media_comment(post.id, 'Nice one!')
+    if not (posts := client.user_medias(user.pk, amount=COUNT_POSTS)):
+        for post in posts:
+            client.media_comment(post.id, 'Nice one!')
     logger.info(f'User: {user}')
